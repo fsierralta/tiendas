f842450464fca5a86aaf4b555644719503cc51d1
fsierralta <sierralta.freddy@gmail.com>
2026-03-12 06:52:23 -0400

    modified:   app/Http/Requests/StorePagoRequest.php

--- app/Http/Requests/StorePagoRequest.php
+++ app/Http/Requests/StorePagoRequest.php
@@ -9,12 +9,7 @@ class StorePagoRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-      return true;
-    //
-      //   return $this->user()->hasRole('jefe') || $this->user()->hasRole('admin');
-    }
+   
 
     /**
      * Get the validation rules that apply to the request.
