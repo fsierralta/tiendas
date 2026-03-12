88de2b66f2396c04ad8d0251ff6442a1db6da772
fsierralta <sierralta.freddy@gmail.com>
2026-03-12 06:56:34 -0400

    modified:   app/Http/Requests/StorePagoRequest.php

--- app/Http/Requests/StorePagoRequest.php
+++ app/Http/Requests/StorePagoRequest.php
@@ -6,13 +6,9 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StorePagoRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
    
 
-    /**
-     * Get the validation rules that apply to the request.
+    /* Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
